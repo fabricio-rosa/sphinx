9a8486edca1d5d9f2b37985aee9ef8b5a5e1fd71
Ubuntu <ubuntu@ip-172-31-12-85.sa-east-1.compute.internal>
2023-04-06 13:23:44 +0000

    initial commit

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. Documentation of Life documentation master file, created by
+   sphinx-quickstart on Thu Apr  6 12:13:06 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Documentation of Life's documentation!
+=================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Documentation of Life'
+copyright = '2023, Fabrício Villas Bôas Rosa'
+author = 'Fabrício Villas Bôas Rosa'
+release = '2023'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'pt_BR'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
